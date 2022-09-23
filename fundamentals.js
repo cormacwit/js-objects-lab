2fae6d50683af7d89fb6c30521d9c0ae4e706854
cormacwit <20093840@mail.wit.ie>
2022-09-23 16:05:00 +0100

    Initial structure.

--- fundamentals.js
+++ fundamentals.js
@@ -2,9 +2,14 @@ const aCar = {
     owner : "Joe Bloggs",
     make: "Toyota",
     model:"Corolla",
-    cc:"1.8",
-    registration : '201WD1058'
+    cc:1.8,
+    registration : {
+    year:201,
+    county_code:"WD",
+    number:1058
+    }
   };
   
   console.log(aCar.owner + ' drives a ' + aCar.make)
+  console.log('The cars reg no is ' + aCar.registration.year + "-" + aCar.registration.county_code + "-" + aCar.registration.number);
   
