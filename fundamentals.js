a1671b79eb8a80f0a149def33459eec654aad6a7
cormacwit <20093840@mail.wit.ie>
2022-09-23 16:29:04 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -9,6 +9,14 @@ const aCar = {
     number:1058
     }
   };
+
+  aCar.milage = 10000
+  aCar.color = {
+    exterior: "red",
+    interior: { texture: "leather", shade: "cream" }
+  }
+
   console.log(aCar.owner + ' drives a ' + aCar.make)
   console.log('The cars reg no is ' + aCar.registration.year + "-" + aCar.registration.county_code + "-" + aCar.registration.number);
-  
+  console.log(
+    "It is a " + aCar.color.exterior + " car, " + aCar.milage + " milage, with " + aCar.color.interior.texture + " interior.");
